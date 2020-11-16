c98ceada4515aafb47e25a9f82502c88a142c4cc
DERUINO <main@deruwork.ru>
2020-11-16 17:17:43 +0300

    final-version

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <title>Site</title>
 
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/fonts/OpenSans/opensans.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
 
     <script type="text/javascript" src="js/jquery.js"></script>
@@ -16,8 +17,120 @@
   </head>
 
   <body>
-
-    
+    <div class="wrapper">
+      <div class="lr-block">
+        <div class="logoblock">
+          <img src="css/images/logo.svg">
+        </div>
+        <div class="title">
+          Продажа запчастей для стайлинга<br>автомобилей Land Rover
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-5 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+            <div class="linkblock">
+              <a href="#">
+                <div class="link_img">
+                  <img src="css/images/link1.svg">
+                </div>
+                <div class="link_text">
+                  Промокод<br>на скидку 6%
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="col-5 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+            <div class="linkblock">
+              <a href="#">
+                <div class="link_img">
+                  <img src="css/images/link2.svg">
+                </div>
+                <div class="link_text">
+                  Полный<br>ассортимент
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-5 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+            <div class="linkblock">
+              <a href="#">
+                <div class="link_img">
+                  <img src="css/images/link3.svg">
+                </div>
+                <div class="link_text">
+                  Отзывы<br>автовладельцев
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="col-5 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+            <div class="linkblock">
+              <a href="#">
+                <div class="link_img">
+                  <img src="css/images/link4.svg">
+                </div>
+                <div class="link_text">
+                  Написать нам<br>в WhatsApp
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-5 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+            <div class="linkblock">
+              <a href="#">
+                <div class="link_img">
+                  <img src="css/images/link5.svg">
+                </div>
+                <div class="link_text">
+                  Написать нам<br>в Телеграм
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="col-5 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+            <div class="linkblock">
+              <a href="#">
+                <div class="link_img">
+                  <img src="css/images/link6.svg">
+                </div>
+                <div class="link_text">
+                  Написать нам<br>в Viber
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-5 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+            <div class="linkblock">
+              <a href="#">
+                <div class="link_img">
+                  <img src="css/images/link7.svg">
+                </div>
+                <div class="link_text">
+                  Видео обзоры<br>чип-тюнинга,<br>обвесов и тд.
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="col-5 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+            <div class="linkblock">
+              <a href="#">
+                <div class="link_img">
+                  <img src="css/images/link8.svg">
+                </div>
+                <div class="link_text">
+                  Полный<br>ассортимент + б/у<br>запчасти
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <script type="text/javascript" src="js/script.js"></script>
   </body>
